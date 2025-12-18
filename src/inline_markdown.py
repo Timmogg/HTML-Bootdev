2729fec818a2e78fa8d02b63b08d61a43ca59262
Timmogg <timothymcpheron8@gmail.com>
2025-12-18 18:23:38 -0500

    add all the split functions for inline_markdown.py and some tests for them.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -26,3 +26,87 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)         
+            continue
+
+        images = extract_markdown_images(old_node.text)
+
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        original_text = old_node.text
+        for image in images:
+            image_alt = image[0]
+            image_link = image[1]
+            sections = original_text.split(f"![{image_alt}]({image_link})", 1)
+
+            before = sections[0]
+            after = sections[1]
+
+            if before != "":
+                new_nodes.append(TextNode(before, TextType.TEXT))
+
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            original_text = after
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        links = extract_markdown_links(old_node.text)
+
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        original_text = old_node.text
+        for link in links:
+            link_text = link[0]
+            link_url = link[1]
+            sections = original_text.split(f"[{link_text}]({link_url})", 1)
+
+            before = sections[0]
+            after = sections[1]
+
+            if before != "":
+                new_nodes.append(TextNode(before, TextType.TEXT))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+
+            original_text = after
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+        
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+    nodes = split_nodes_image(nodes)
+
+    nodes = split_nodes_link(nodes)
+
+    return nodes
+    
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -3,7 +3,10 @@ import re
 from inline_markdown import (
     split_nodes_delimiter,
     extract_markdown_images,
-    extract_markdown_links
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
@@ -95,8 +98,31 @@ class TestInlineMarkdown(unittest.TestCase):
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
-
-
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    def test_text_to_textnodes_bold(self):
+        text = "This is **bold** text"
+        result = text_to_textnodes(text)
+        assert result == [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT),
+        ]
 if __name__ == "__main__":
     unittest.main()
 
