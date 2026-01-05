7f205db8b322f0e5f0c895dac55640096f64f58e
Timmogg <timothymcpheron8@gmail.com>
2026-01-04 20:28:10 -0500

    build html website using github, add build.sh and update gencontent.py and main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,30 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
 dir_path_content = "./content"
 template_path = "./template.html"
+dir_path_docs = "./docs"
 
 def main():
+    if len(sys.argv) >= 2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(basepath, dir_path_content, template_path, dir_path_docs)
 
 main()    
 
